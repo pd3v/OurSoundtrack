7216b34a845e7f06db5cff2e05dc976b06ea160a
pd3v <paulo.develop@gmail.com>
2018-01-11 21:13:12 +0000

    client audio receiving mod data from server

--- public/client.js
+++ public/client.js
@@ -1,39 +1,59 @@
+var feedbackDelayObj = new Tone.PingPongDelay({
+  "delayTime" : "0.1",
+  "feedback" : 0.25,
+  "wet" : 0.25
+}).toMaster();
+
+var player = new Tone.Player("egtr.wav").connect(feedbackDelayObj);
+
+player.autostart = true;
+player.loop = true;
 
 var socket = new WebSocket('ws://localhost:8081/');
 socket.onopen = function(event) {
-  log('Opened connection 🎉');
+  //log('Opened connection 🎉');
   var json = JSON.stringify({ message: 'Hello' });
   socket.send(json);
-  log('Sent: ' + json);
+  //log('Sent: ' + json);
 }
 
 socket.onerror = function(event) {
-  log('Error: ' + JSON.stringify(event));
+  //log('Error: ' + JSON.stringify(event));
 }
 
 socket.onmessage = function (event) {
-  log('Received: ' + event.data);
+  //log('Received: ' + event.data);
+  modulation(event.data);
 }
 
 socket.onclose = function(event) {
-  log('Closed connection 😱');
+  //log('Closed connection 😱');
 }
 
 document.querySelector('#close').addEventListener('click', function(event) {
   socket.close();
-  log('Closed connection 😱');
+  //log('Closed connection 😱');
 });
 
 document.querySelector('#send').addEventListener('click', function(event) {
-  var json = JSON.stringify({ message: 'Hey there' });
+  //var json = JSON.stringify({ message: 'Hey there' });
+  var json = JSON.stringify({ message: cont++ });
   socket.send(json);
-  log('Sent: ' + json);
+  //log('Sent: ' + json);
 });
 
 var log = function(text) {
   var li = document.createElement('li');
   li.innerHTML = text;
   document.getElementById('log').appendChild(li);
+}
+
+var modulation = function(text){
+    var modulateServerStream = JSON.parse(text);
+    feedbackDelayObj.feedback.value = modulateServerStream.message;
+    
+    document.getElementById('modulation').innerHTML = modulateServerStream.message;
+    document.getElementById('numClients').innerHTML = modulateServerStream.numClients;
 }
 
 window.addEventListener('beforeunload', function() {
--- server.js
+++ server.js
@@ -7,38 +7,48 @@ var app = express().use(express.static('public'));
 var server = http.createServer(app);
 server.listen(8080, '127.0.0.1');
 
+var fxParam = 0.33;
+var numClients = 0;
+
 var wss = new WSS({ port: 8081 });
 wss.on('connection', function(socket) {
-  console.log('Opened Connection 🎉');
+  //console.log('Opened Connection 🎉');
 
-  var json = JSON.stringify({ message: 'Gotcha' });
+  //var json = JSON.stringify({ message: 'Gotcha' });
+  var json = JSON.stringify({ message: fxParam });
+  
   socket.send(json);
-  console.log('Sent: ' + json);
 
   socket.on('message', function(message) {
-    console.log('Received: ' + message);
-
+    numClients = wss.clients.length;
     wss.clients.forEach(function each(client) {
-      var json = JSON.stringify({ message: 'Something changed' });
+      //var json = JSON.stringify({ message: 'Something changed' });
+      
+      var json = JSON.stringify({ 
+        message: fxParam,
+        numClients: numClients
+      });
+      
       client.send(json);
-      console.log('Sent: ' + json);
+      //console.log('Sent: ' + json);
     });
   });
 
   socket.on('close', function() {
-    console.log('Closed Connection 😱');
+    //console.log('Closed Connection 😱');
   });
-
 });
 
 var broadcast = function() {
   var json = JSON.stringify({
-    message: 'Hello hello!'
+    //message: 'Hello hello!'
+    message: Math.random(),
+    numClients: numClients
   });
-
+  //console.log("broadcast:",json);
   wss.clients.forEach(function each(client) {
     client.send(json);
-    console.log('Sent: ' + json);
+    //console.log('Sent: ' + json);
   });
 }
-setInterval(broadcast, 3000);
+setInterval(broadcast, Math.random()*5000);
