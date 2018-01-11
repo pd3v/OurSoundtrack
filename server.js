1cdc70671ae76f39f754b40e7fb5e43a76daaa68
pd3v <paulo.develop@gmail.com>
2018-01-11 23:22:34 +0000

    set port dinamically

--- server.js
+++ server.js
@@ -2,53 +2,55 @@
 var http = require('http');
 var express = require('express');
 var WSS = require('ws').Server;
-
+const PORT = process.env.PORT || 8080
 var app = express().use(express.static('public'));
 var server = http.createServer(app);
-server.listen(8080, '127.0.0.1');
 
-var fxParam = 0.33;
-var numClients = 0;
-
-var wss = new WSS({ port: 8081 });
-wss.on('connection', function(socket) {
-  //console.log('Opened Connection 🎉');
-
-  //var json = JSON.stringify({ message: 'Gotcha' });
-  var json = JSON.stringify({ message: fxParam });
+server.listen(PORT, function(){
   
-  socket.send(json);
+  var fxParam = 0.33;
+  var numClients = 0;
 
-  socket.on('message', function(message) {
-    numClients = wss.clients.length;
-    wss.clients.forEach(function each(client) {
-      //var json = JSON.stringify({ message: 'Something changed' });
-      
-      var json = JSON.stringify({ 
-        message: fxParam,
-        numClients: numClients
+  var wss = new WSS({ port: 8081 });
+  wss.on('connection', function(socket) {
+    //console.log('Opened Connection 🎉');
+
+    //var json = JSON.stringify({ message: 'Gotcha' });
+    var json = JSON.stringify({ message: fxParam });
+    
+    socket.send(json);
+
+    socket.on('message', function(message) {
+      numClients = wss.clients.length;
+      wss.clients.forEach(function each(client) {
+        //var json = JSON.stringify({ message: 'Something changed' });
+        
+        var json = JSON.stringify({ 
+          message: fxParam,
+          numClients: numClients
+        });
+        
+        client.send(json);
+        //console.log('Sent: ' + json);
       });
-      
-      client.send(json);
-      //console.log('Sent: ' + json);
+    });
+
+    socket.on('close', function() {
+      //console.log('Closed Connection 😱');
     });
   });
 
-  socket.on('close', function() {
-    //console.log('Closed Connection 😱');
-  });
+  var broadcast = function() {
+    var json = JSON.stringify({
+      //message: 'Hello hello!'
+      message: Math.random(),
+      numClients: numClients
+    });
+    //console.log("broadcast:",json);
+    wss.clients.forEach(function each(client) {
+      client.send(json);
+      //console.log('Sent: ' + json);
+    });
+  }
+  setInterval(broadcast, Math.random()*5000);
 });
-
-var broadcast = function() {
-  var json = JSON.stringify({
-    //message: 'Hello hello!'
-    message: Math.random(),
-    numClients: numClients
-  });
-  //console.log("broadcast:",json);
-  wss.clients.forEach(function each(client) {
-    client.send(json);
-    //console.log('Sent: ' + json);
-  });
-}
-setInterval(broadcast, Math.random()*5000);
